0a4103b8414411de7e327f0c6fe469a60d25711e
Pham-Gia-Bao1 <phamgiabao123abc@gmail.com>
2023-09-20 16:14:36 +0700

    bao add full

---
+++ point/main.js
@@ -0,0 +1,39 @@
+function info(name,age,address){
+    this.name = name;
+    this.age= age ;  //this is the object that we are creating
+    this.address = address;
+}
+
+function search(){
+    var arr = [
+        {
+            name : "bao",
+            age : 12,
+            
+        },
+        {
+            name : "nam",
+            age : 12,
+            
+        },{
+            name : "anh",
+            age : 12,
+            
+        },{
+            name : "mai",
+            age : 12,
+            
+        },{
+            name : "long",
+            age : 12,
+            
+        },{
+            name : "hung",
+            age : 12,
+            
+        },
+    ]
+    arr.find(function() {
+        
+    })
+}
